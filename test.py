ab9bee995a78e1eb4cdbdf688c68f0e22f9289b1
Dali Brahem <36166990+dlightsolution@users.noreply.github.com>
2023-01-17 09:39:55 +0100

    Update test.py

--- test.py
+++ test.py
@@ -12,14 +12,20 @@ def test_addresses():
         words = MyFunctions.split_address(row['address'])
         idx = MyFunctions.parse_words(words)
         data = MyFunctions.assign_values(idx, words)
-        print({"street": row["streetname"], "housenumber": row["housenumber"]})
-        #Check if data in csv is equal to the expected output
+        #print(data)
+        #print({"street": row["streetname"], "housenumber": row["housenumber"]})
+        #This will compare the json output from the main.py file with the expected output
         if data == {"street": row["streetname"], "housenumber": row["housenumber"]}:
             positiveaddresses.append(row["address"])
         else:
             negativeaddresses.append(row["address"])
 
-    #Print the negative addresses
+    #print the number of negative addresses
     print(len(negativeaddresses),"/",len(df)," has failed the test")
+    #Print the negative addresses if found
+    if len(negativeaddresses) > 0:
+        print("The following addresses have failed the test:", negativeaddresses)
+    else:
+        print("Alles gut!")
         
 print(test_addresses())
